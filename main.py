920cad450561527bb4b5128f86c35f0bd22095d8
AhmedNadhemHasan <ahmed.nadhem.hasan@gmail.com>
2024-10-16 22:40:21 -0700

    added, updated and calculated parameters

--- main.py
+++ main.py
@@ -12,15 +12,52 @@ def create_table():
             PlayerName TEXT NOT NULL,
             PlayerNumber INT NOT NULL PRIMARY KEY,
             X2PM INT DEFAULT 0,
-            X3PM INT DEFAULT 0,
             X2PA INT DEFAULT 0 ,
+            X2PP FLOAT DEFAULT 0,       
+            X3PM INT DEFAULT 0,
             X3PA INT DEFAULT 0,
-            FTM DEFAULT 0,
-            FTA DEFAULT 0 ,
+            X3PP FLOAT DEFAULT 0,
+            FGA INT DEFAULT 0,
+            FGM INT DEFAULT 0,
+            FGP FLOAT DEFAULT 0,
+            FTM INT DEFAULT 0,
+            FTA INT DEFAULT 0 ,
+            FTP FLOAT DEFAULT 0,
+            TOTAL_REBS INT DEFAULT 0,
+            DR INT DEFAULT 0,
+            OR INT DEFAULT 0,
+            FOULS INT DEFAULT 0,
+            FD INT DEFAULT 0,
+            PF INT DEFAULT 0,
+            ASSISTS INT DEFAULT 0,
+            STEALS INT DEFAULT 0,
+            TO INT DEFAULT 0,
+            PTS INT DEFAULT 0,
                    CHECK (X2PA>=X2PM),
                    CHECK (X3PA>=X3PM),
                    CHECK (FTA>=FTM)
-            )         
+            );
+    UPDATE playerStats
+    SET X2PP = ROUND(CAST(X2PM AS DECIMAL)/CAST(X2PA AS DECIMAL)*100,1);
+    
+    UPDATE playerStats
+    SET X3PP = ROUND(CAST(X3PM AS DECIMAL)/CAST(X3PA AS DECIMAL)*100,1);
+                   
+    UPDATE playerStats
+    SET FGA = X2PA + X3PA;
+    
+    UPDATE playerStats
+    SET FGM = X2PM + X3PM;
+        
+    UPDATE playerStats
+    SET FTP = ROUND(CAST(FTM AS DECIMAL)/CAST(FTA AS DECIMAL)*100,1);
+    
+    UPDATE playerStats
+    SET FGP = ROUND(CAST(FGM AS DECIMAL)/CAST(FGA AS DECIMAL)*100,1);
+                   
+    UPDATE playerStats
+    SET PTS = (2*(X2PM)) + (3*(X3PM));
+                     
     ''')
     conn.commit()
 
