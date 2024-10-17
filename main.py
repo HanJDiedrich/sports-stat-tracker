88fbc34f7190dcf3c6f36c5e17ca1d8292504b9e
AhmedAlrashid <ahmedalrashiddd8@gmail.com>
2024-10-16 23:01:06 -0700

    created functions for calculated columns and fixed errors

--- main.py
+++ main.py
@@ -24,42 +24,53 @@ def create_table():
             FTA INT DEFAULT 0 ,
             FTP FLOAT DEFAULT 0,
             TOTAL_REBS INT DEFAULT 0,
-            DR INT DEFAULT 0,
-            OR INT DEFAULT 0,
+            Defensive_Rebs INT DEFAULT 0,
+            Offensive_Rebs INT DEFAULT 0,
             FOULS INT DEFAULT 0,
             FD INT DEFAULT 0,
             PF INT DEFAULT 0,
             ASSISTS INT DEFAULT 0,
             STEALS INT DEFAULT 0,
-            TO INT DEFAULT 0,
+            TurnOver INT DEFAULT 0,
             PTS INT DEFAULT 0,
                    CHECK (X2PA>=X2PM),
                    CHECK (X3PA>=X3PM),
                    CHECK (FTA>=FTM)
-            );
-    UPDATE playerStats
-    SET X2PP = ROUND(CAST(X2PM AS DECIMAL)/CAST(X2PA AS DECIMAL)*100,1);
-    
-    UPDATE playerStats
-    SET X3PP = ROUND(CAST(X3PM AS DECIMAL)/CAST(X3PA AS DECIMAL)*100,1);
-                   
-    UPDATE playerStats
-    SET FGA = X2PA + X3PA;
-    
-    UPDATE playerStats
-    SET FGM = X2PM + X3PM;
-        
-    UPDATE playerStats
-    SET FTP = ROUND(CAST(FTM AS DECIMAL)/CAST(FTA AS DECIMAL)*100,1);
-    
-    UPDATE playerStats
-    SET FGP = ROUND(CAST(FGM AS DECIMAL)/CAST(FGA AS DECIMAL)*100,1);
-                   
-    UPDATE playerStats
-    SET PTS = (2*(X2PM)) + (3*(X3PM));
-                     
-    ''')
+            );    ''')
     conn.commit()
+    def Update_2PP():
+        cursor.execute('''UPDATE playerStats
+    SET X2PP = ROUND(CAST(X2PM AS DECIMAL)/CAST(X2PA AS DECIMAL)*100,1);''')
+    
+    def Update_3PP():
+        cursor.execute('''UPDATE playerStats
+     SET X3PP = ROUND(CAST(X3PM AS DECIMAL)/CAST(X3PA AS DECIMAL)*100,1);''')
+
+    def Update_FGA():
+        cursor.execute('''UPDATE playerStats
+     SET FGA = X2PA + X3PA;''')
+        
+    def Update_FGM():
+        cursor.execute('''UPDATE playerStats
+     SET FGM = X2PM + X3PM;''')
+        
+    def Update_rebs():
+        cursor.execute('''UPDATE playerStats
+    SET Total_rebs=Defensive_rebs+Offensive_rebs;''')
+        
+    def Update_FTP():
+        cursor.execute('''UPDATE playerStats
+     SET FTP = ROUND(CAST(FTM AS DECIMAL)/CAST(FTA AS DECIMAL)*100,1);''')
+        
+    def Update_FGP():
+        cursor.execute('''UPDATE playerStats
+     SET FGP = ROUND(CAST(FGM AS DECIMAL)/CAST(FGA AS DECIMAL)*100,1);''')
+
+    def Update_Pts():
+        cursor.execute('''UPDATE playerStats
+     SET PTS = (2*(X2PM)) + (3*(X3PM));''')
+                     
+
 
 def insert_player_stats(player_name, player_number, made_2pt, made_3pt, att_2pt, att_3pt, ft_made, ft_attempted):
     cursor.execute('''
@@ -67,9 +78,9 @@ def insert_player_stats(player_name, player_number, made_2pt, made_3pt, att_2pt,
         VALUES (?, ?, ?, ?, ?, ?, ?, ?)
     ''', (player_name, player_number, made_2pt, made_3pt, att_2pt, att_3pt, ft_made, ft_attempted))
     conn.commit()
-create_table()
+#create_table()
 # Example of inserting a player using the function
-insert_player_stats('John Doe', 23, 5, 2, 10, 5, 4, 6)
+#insert_player_stats('John Doe', 23, 5, 2, 10, 5, 4, 6)
 
 # Fetch and print all data
 cursor.execute("SELECT * FROM playerStats")
